d65eee1962db81833714ee30889fb194d10cf989
Georgi Dimitrov <georgi.dimitrov@delta.bg>
2021-03-10 09:01:40 +0200

    Extract entries in entryComponent

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,41 @@
+const entryComponent = {
+    template:
+    `<div style="display: flex; width: 100%">
+        <figure class="media-left">
+            <img class="image is-64x64" v-bind:src="entry.image" />
+        </figure>
+        <div class="media-content">
+            <div class="content">
+                <p>
+                    <strong>
+                        <a v-bind:href="entry.url" class="has-text-info">{{ entry.title }}</a>
+                        <span class="tag is-small">#{{ entry.id }}</span>
+                    </strong>
+                    <br>
+                    {{ entry.description }}
+                    <br>
+                    <small class="is-size-7">Added by:
+                        <img class="image is-24x24" v-bind:src="entry.avatar">
+                    </small>
+                </p>
+            </div>
+        </div>
+        <div class="media-right">
+            <span class="icon is-small" v-on:click="upvote(entry.id)">
+                <i class="fa fa-chevron-up"></i>
+                <strong class="has-text-info">{{ entry.votes}}</strong>
+            </span>
+        </div>
+    </div>`,
+    props: ['entry', 'entries'],
+    methods: {
+        upvote(entryId) {
+            const entry = this.entries.find(entry => entry.id === entryId);
+            entry.votes++;
+        }
+    },
+};
+
 new Vue({
     el: '#app',
     data: {
@@ -11,10 +49,7 @@ new Vue({
             });
         }
     },
-    methods: {
-        upvote(entryId) {
-            const entry = this.entries.find(entry => entry.id === entryId);
-            entry.votes++;
-        }
+    components: {
+        'entry-component': entryComponent
     }
 });
