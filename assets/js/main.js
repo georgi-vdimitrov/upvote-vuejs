db7bd7577666e099e51eb96bc26c69ba98333169
Georgi Dimitrov <georgi.dimitrov@delta.bg>
2021-03-10 08:54:19 +0200

    Added vote up method and new styling when votes are gte 20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-new Vue ({
-   el: '#app',
+new Vue({
+    el: '#app',
     data: {
-       title: 'This is the UpVote App!',
+        title: 'This is the UpVote App!',
         entries: Seed.entries
+    },
+    computed: {
+        sortedEntries() {
+            return this.entries.sort((a, b) => {
+                return b.votes - a.votes;
+            });
+        }
+    },
+    methods: {
+        upvote(entryId) {
+            const entry = this.entries.find(entry => entry.id === entryId);
+            entry.votes++;
+        }
     }
 });
